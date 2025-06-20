d6ff6dfc1ae7295d163971ca0abcbc74b479a6f0
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-06-20 14:02:40 -0300

    add material ui

---
+++ app/theme.js
@@ -0,0 +1,17 @@
+// src/theme.js
+'use client'; // Marque como Client Component
+
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
+
+export default theme;
