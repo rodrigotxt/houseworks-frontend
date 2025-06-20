11939f9960d736fde3f6f5c47f264ef0d6472a9b
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-06-20 03:54:58 -0300

    add MUI

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css'; // Estilos globais
 import App from './App'; // Seu componente principal App
 import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
