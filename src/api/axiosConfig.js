236f7853f899928751d3b8e9f79178945a1d1f68
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-06-20 04:36:47 -0300

    install next.js

--- src/api/axiosConfig.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api' // Use a URL da API, ou /api se estiver no mesmo domínio
-});
-export default instance;
--- src/pages/TaskPage.js
+++
@@ -1,8 +0,0 @@
-function TaskPage() {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
-}
-export default TaskPage;
