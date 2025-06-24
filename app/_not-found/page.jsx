583a08e4bec5768523cb2e63434c9bef05a144df
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-06-24 03:35:13 -0300

    add default pages

---
+++ app/_not-found/page.jsx
@@ -0,0 +1,10 @@
+// _not-found/page.jsx
+
+export default function NotFound() {
+    return (
+        <main>
+            <h1>404 - Not Found</h1>
+            <h3>Página não encontrada.</h3>
+        </main>
+    );
+}
